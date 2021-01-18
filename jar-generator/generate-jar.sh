d46ab3bf31cdb19de7d8ac3bf94125c885d87249
Miroslav Mazel <contact.mirek@protonmail.com>
2021-01-18 23:40:51 +0100

    JAR generation script tweaks

--- jar-generator/generate-jar.sh
+++ jar-generator/generate-jar.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-processing-java --force --sketch=`pwd`/../fosstriangulator --output=`pwd`/out --export --platform='linux' #todo USE VARIABLE IN PATHS INSTEAD!
-cd ./out/application.linux64/lib/ #todo USE VARIABLE IN PATHS INSTEAD!
-unzip -o controlP5.jar -d fosstriangulator
-unzip -o core.jar -d fosstriangulator
-unzip -o itext.jar -d fosstriangulator
-unzip -o pdf.jar -d fosstriangulator
-unzip -o fosstriangulator.jar -d fosstriangulator
-cp ../../../jar-generator/icons/* ./fosstriangulator/icon/
-zip -r ../../fosstriangulator.jar ./fosstriangulator/*
+~/lib/processing-3.5.4/processing-java --force --sketch=`pwd`/../../FOSStriangulator --output=`pwd`/out --export --platform='linux' #todo USE VARIABLE IN PATHS INSTEAD!
+cd ./out/lib/ #todo USE VARIABLE IN PATHS INSTEAD!
+unzip -o controlP5.jar -d FOSStriangulator
+unzip -o core.jar -d FOSStriangulator
+unzip -o itext.jar -d FOSStriangulator
+unzip -o pdf.jar -d FOSStriangulator
+unzip -o FOSStriangulator.jar -d FOSStriangulator
+cp ../../../jar-generator/icons/* ./FOSStriangulator/icon/
+cd FOSStriangulator
+zip -r ../../../FOSStriangulator.jar *
